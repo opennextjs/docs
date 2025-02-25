b15bebea94ee63dbf4ad85b4178115457d7d0fd8
James <james@eli.cx>
2025-02-25 20:52:47 +0000

    chore: add formatter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,7 @@ import styles from "../styles/Layout.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span>
-        Maintained by the OpenNext community
-      </span>
+      <span>Maintained by the OpenNext community</span>
       <div>
         <a target="_blank" href={SITE.github} rel="noopener noreferrer">
           GitHub
